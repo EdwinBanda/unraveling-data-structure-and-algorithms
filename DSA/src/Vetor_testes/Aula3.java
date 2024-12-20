4ffdfa27af7cc2087bf8e5692bf15a9cfdf3787a
EdwinBanda <casemirobanda@gmail.com>
2024-12-20 10:35:49 +0200

    feat: adding elements inside a vector

---
+++ DSA/src/Vetor_testes/Aula3.java
@@ -0,0 +1,16 @@
+package Vetor_testes;
+
+import vetor.Vetor;
+
+public class Aula3 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(2);
+    try {
+        vetor.adding("Elemento 1");
+        vetor.adding("Elemento 2");
+        vetor.adding("Elemento 3");
+    }catch (Exception e){
+        e.printStackTrace();
+    }
+    }
+}
--- DSA/src/vetor/Vetor.java
+++ DSA/src/vetor/Vetor.java
@@ -2,8 +2,27 @@ package vetor;
 
 public class Vetor {
     private String[] elementos;
+    private int arrSize;
 
     public Vetor(int capacidade){
         this.elementos = new String[capacidade];
+        this.arrSize = 0;
+    }
+    //Adding elements inside a vector
+//    public void adding(String element){
+//        for(int i =0; i < elementos.length; i++){
+//            if(elementos[i] == null){
+//                this.elementos[i] = element;
+//                break;
+//            }
+//        }
+//    }
+    public void adding(String element) throws Exception{
+        if(this.arrSize < this.elementos.length){
+            this.elementos[this.arrSize] = element;
+            this.arrSize++;
+        }else{
+            throw new Exception("Vetor is full!");
+        }
     }
 }
