250633ecb78d2d805621a2efe13ccb79972683cb
EdwinBanda <casemirobanda@gmail.com>
2024-12-21 10:06:27 +0200

    feat: get the real size of array without counting blank spaces

--- DSA/src/Vetor_testes/SizeOfArray.java
+++ DSA/src/Vetor_testes/SizeOfArray.java
@@ -1,4 +1,18 @@
 package Vetor_testes;
 
+import vetor.Vetor;
+
 public class SizeOfArray {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(10);
+        try {
+            vetor.adding("Elemento 1");
+            vetor.adding("Elemento 2");
+            vetor.adding("Elemento 3");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(vetor.arrsize());
+        System.out.println(vetor.toString());
+    }
 }
