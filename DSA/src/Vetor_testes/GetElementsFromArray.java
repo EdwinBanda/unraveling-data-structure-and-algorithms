da672226aa71422396d8a424668091a3fcd7eb89
EdwinBanda <casemirobanda@gmail.com>
2024-12-21 10:05:09 +0200

    feat: Get elements from array

--- DSA/src/Vetor_testes/GetElementsFromArray.java
+++ DSA/src/Vetor_testes/GetElementsFromArray.java
@@ -1,4 +1,18 @@
 package Vetor_testes;
 
+import vetor.Vetor;
+
 public class GetElementsFromArray {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(10);
+        try {
+            vetor.adding("Elemento 1");
+            vetor.adding("Elemento 2");
+            vetor.adding("Elemento 3");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(vetor.getElement(2));
+    }
 }
+
