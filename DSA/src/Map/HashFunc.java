9dbbeed2a4d9d9c6548e81f3281785f894a9bd9c
EdwinBanda <casemirobanda@gmail.com>
2025-02-12 09:45:35 +0200

    feat: implements the hash function logic using bitwise operators (left shift and right shift)

---
+++ DSA/src/Map/HashFunc.java
@@ -0,0 +1,20 @@
+package Map;
+
+public class HashFunc {
+    public static int hasher(String s){
+        int h = 0;
+        for(int i = 0; i < s.length(); i++){
+            h = (h << 5) | (h >>> 27);
+            h += (int) s.charAt(i);
+        }
+        return h;
+    }
+
+    public static void main(String[] args) {
+        int myCode = hasher("1");
+        int another = hasher("5");
+        System.out.println(myCode);
+        System.out.println(another);
+        System.out.println(myCode == another);
+    }
+}
